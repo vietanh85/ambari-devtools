934ec9b7e0f626466ba98b2aace4304cdb767ffa
Jonathan Hurley <jonathanhurley@apache.org>
2019-02-07 12:38:07 -0500

    Removing requirements on postgres user and switching to direct psql calls

--- server-init.sh
+++ server-init.sh
@@ -12,12 +12,12 @@ echo "${NC}"
 export KEYSTORE_ROOT=$AMBARI_HOME/keystore
 
 echo "${CYAN}-*- Dropping old database...${NC}"
-sudo -u postgresql psql password=password -f $AMBARI_GIT/ambari-server/src/main/resources/Ambari-DDL-Postgres-EMBEDDED-DROP.sql -v dbname="ambari"
+psql password=password -f $AMBARI_GIT/ambari-server/src/main/resources/Ambari-DDL-Postgres-EMBEDDED-DROP.sql -v dbname="ambari"
 echo ""
 
 echo "${CYAN}-*- Creating new database...${NC}"
-sudo -u postgres psql password=password -f $AMBARI_GIT/ambari-server/src/main/resources/Ambari-DDL-Postgres-EMBEDDED-CREATE.sql -v username="\"ambari\"" -v password=\'bigdata\' -v dbname=ambari
-sudo -u postgres psql -U ambari password=bigdata -f $AMBARI_GIT/ambari-server/src/main/resources/Ambari-DDL-Postgres-CREATE.sql
+psql password=password -f $AMBARI_GIT/ambari-server/src/main/resources/Ambari-DDL-Postgres-EMBEDDED-CREATE.sql -v username="\"ambari\"" -v password=\'bigdata\' -v dbname=ambari
+psql -U ambari password=bigdata -f $AMBARI_GIT/ambari-server/src/main/resources/Ambari-DDL-Postgres-CREATE.sql
 echo ""
 
 echo "${CYAN}-*- Resetting certificates...${NC}"
@@ -31,19 +31,20 @@ sed -i s,/var/lib/ambari-server/keys/db,/Users/$USERNAME/dev/ambari/keystore/db,
 clearMpackV2Artifacts
 
 echo
-printf "${YELLOW}Do you want to link the HDP Stack from Gerrit to Ambari Server (Ambari 2.7)? [Y/N] (N):${NC} "
+printf "${YELLOW}Do you want to link the HDP Stack from Gerrit to Ambari Server? [Y/N] (Y):${NC} "
 read -r LINK_HDP_STACK
 if [[ -z "${LINK_HDP_STACK// }" ]]; then
-  LINK_HDP_STACK="N"
+  LINK_HDP_STACK="Y"
 fi
 
 # remove the stacks symlink or directory
 PRIVATE_RESOURCES_STACKS=/private/ambari-server/resources/stacks
+AMBARI_SRC_STACKS=$AMBARI_GIT/ambari-server/src/main/resources/stacks
 PRIVATE_RESOURCES_STACKS_HDP=${PRIVATE_RESOURCES_STACKS}/HDP
 if [[ -d "${PRIVATE_RESOURCES_STACKS}" ]]; then
   echo "${CYAN}-*- Removing the ${RED}directory${CYAN} ${PRIVATE_RESOURCES_STACKS} ${NC}"
   rm -rf ${PRIVATE_RESOURCES_STACKS}
-elif [[ condition ]]; then
+elif [[ -L "${PRIVATE_RESOURCES_STACKS}" ]]; then
   echo "${CYAN}-*- Removing the ${RED}symlink${CYAN} ${PRIVATE_RESOURCES_STACKS} ${NC}"
   rm -f ${PRIVATE_RESOURCES_STACKS}
 fi
@@ -52,13 +53,21 @@ case "$LINK_HDP_STACK" in
   [yY])
     echo "${CYAN}-*- Linking HDP Stack from Gerrit...${NC}"
 
-    HDP_VERSIONS=(2.4 2.5 2.6 2.7 2.8 3.0 3.1)
+    # iterate over all known HDP versions
+    HDP_VERSIONS=(2.4 2.5 2.6 2.7 2.8 3.0 3.1,3.2)
 
-    # make the stacks directory
+    # make the stacks directory only if it doesn't exist
     mkdir -p ${PRIVATE_RESOURCES_STACKS_HDP}
 
+    # link everything in source to private so that Python files are found
+    for ENTRY in $AMBARI_SRC_STACKS/*; do
+      FILENAME=${ENTRY##*/}
+      echo "      $PRIVATE_RESOURCES_STACKS/$FILENAME ${GREEN}->${NC} $AMBARI_SRC_STACKS/$FILENAME"
+      ln -s $AMBARI_SRC_STACKS/$FILENAME $PRIVATE_RESOURCES_STACKS/$FILENAME
+    done
+
     # find any HDP versions in Ambari's source tree (normally for older versions)
-    AMBARI_HDP_DIRECTORY=$AMBARI_GIT/ambari-server/src/main/resources/stacks/HDP
+    AMBARI_HDP_DIRECTORY=$AMBARI_SRC_STACKS/HDP
     if [[ -d "${AMBARI_HDP_DIRECTORY}" ]]; then
       for HDP_VERSION in "${HDP_VERSIONS[@]}"
       do
